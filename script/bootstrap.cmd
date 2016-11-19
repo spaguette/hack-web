2c1aaa75d7753352b4e669bc54519bed6c6fa4de
rapoport <ar-rapoport@yandex.ru>
2016-11-19 17:15:42 +0300

    readme & cmd
    исправление скриптов для винды

--- script/bootstrap.cmd
+++ script/bootstrap.cmd
@@ -1,4 +1,2 @@
 echo 'Installing yarn globally'
-npm i yarn@0.17.6 -q -g --no-progress --no-spin
-echo 'Installing project npm dependencies'
-npm run yarn
+npm i yarn@0.17.6 -q -g --no-progress --no-spin && echo 'Installing project npm dependencies' && npm run yarn
