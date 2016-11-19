b6e5a535d7a9dc9102d2ed390fc0fe9fc2d2c3da
rapoport <ar-rapoport@yandex.ru>
2016-11-19 22:38:10 +0300

    cosmetic

--- src/js/components/LoginComponent/mainau.js
+++ src/js/components/LoginComponent/mainau.js
@@ -1,13 +1,13 @@
 /* Copyright 2013 Chris Wilson
 
- Licensed under the Apache License, Version 2.0 (the "License");
+ Licensed under the Apache License, Version 2.0 (the 'License');
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
 
  http://www.apache.org/licenses/LICENSE-2.0
 
  Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
+ distributed under the License is distributed on an 'AS IS' BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
@@ -29,83 +29,86 @@ var recIndex = 0;
 /* TODO:
 
  - offer mono option
- - "Monitor input" switch
+ - 'Monitor input' switch
  */
 
 function saveAudio() {
-    audioRecorder.exportWAV( doneEncoding );
+    audioRecorder.exportWAV(doneEncoding);
     // could get mono instead by saying
     // audioRecorder.exportMonoWAV( doneEncoding );
 }
 
-function gotBuffers( buffers ) {
-    var canvas = document.getElementById( "wavedisplay" );
+function gotBuffers(buffers) {
+    var canvas = document.getElementById('wavedisplay');
 
-    drawBuffer( canvas.width, canvas.height, canvas.getContext('2d'), buffers[0] );
+    drawBuffer(canvas.width, canvas.height, canvas.getContext('2d'), buffers[0]);
 
     // the ONLY time gotBuffers is called is right after a new recording is completed -
     // so here's where we should set up the download.
-    audioRecorder.exportWAV( doneEncoding );
+    audioRecorder.exportWAV(doneEncoding);
 }
-function drawBuffer( width, height, context, data ) {
-    var step = Math.ceil( data.length / width );
+function drawBuffer(width, height, context, data) {
+    var step = Math.ceil(data.length / width);
     var amp = height / 2;
-    context.fillStyle = "silver";
-    context.clearRect(0,0,width,height);
-    for(var i=0; i < width; i++){
+    context.fillStyle = 'silver';
+    context.clearRect(0, 0, width, height);
+    for (var i = 0; i < width; i++) {
         var min = 1.0;
         var max = -1.0;
-        for (var j=0; j<step; j++) {
-            var datum = data[(i*step)+j];
-            if (datum < min)
+        for (var j = 0; j < step; j++) {
+            var datum = data[(i * step) + j];
+            if (datum < min) {
                 min = datum;
-            if (datum > max)
+            }
+            if (datum > max) {
                 max = datum;
+            }
         }
-        context.fillRect(i,(1+min)*amp,1,Math.max(1,(max-min)*amp));
+        context.fillRect(i, (1 + min) * amp, 1, Math.max(1, (max - min) * amp));
     }
 }
 
-function doneEncoding( blob ) {
-    Recorder.setupDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
+function doneEncoding(blob) {
+    Recorder.setupDownload(blob, 'myRecording' + ((recIndex < 10) ? '0' : '') + recIndex + '.wav');
     recIndex++;
 }
 
-export function toggleRecording( e ) {
+export function toggleRecording(e) {
     console.log(e);
-    if (e.target.classList.contains("recording")) {
+    if (e.target.classList.contains('recording')) {
         // stop recording
         audioRecorder.stop();
-        e.target.classList.remove("recording");
-        audioRecorder.getBuffers( gotBuffers );
+        e.target.classList.remove('recording');
+        audioRecorder.getBuffers(gotBuffers);
     } else {
         // start recording
-        if (!audioRecorder)
+        if (!audioRecorder) {
             return;
-        e.target.classList.add("recording");
+        }
+        e.target.classList.add('recording');
         audioRecorder.clear();
         audioRecorder.record();
     }
 }
 
-function convertToMono( input ) {
+function convertToMono(input) {
     var splitter = audioContext.createChannelSplitter(2);
     var merger = audioContext.createChannelMerger(2);
 
-    input.connect( splitter );
-    splitter.connect( merger, 0, 0 );
-    splitter.connect( merger, 0, 1 );
+    input.connect(splitter);
+    splitter.connect(merger, 0, 0);
+    splitter.connect(merger, 0, 1);
     return merger;
 }
 
 function cancelAnalyserUpdates() {
-    window.cancelAnimationFrame( rafID );
+    window.cancelAnimationFrame(rafID);
     rafID = null;
 }
 
 function updateAnalysers(time) {
     if (!analyserContext) {
-        var canvas = document.getElementById("analyser");
+        var canvas = document.getElementById('analyser');
         canvasWidth = canvas.width;
         canvasHeight = canvas.height;
         analyserContext = canvas.getContext('2d');
@@ -128,18 +131,19 @@ function updateAnalysers(time) {
         // Draw rectangle for each frequency bin.
         for (var i = 0; i < numBars; ++i) {
             var magnitude = 0;
-            var offset = Math.floor( i * multiplier );
+            var offset = Math.floor(i * multiplier);
             // gotta sum/average the block, or we miss narrow-bandwidth spikes
-            for (var j = 0; j< multiplier; j++)
+            for (var j = 0; j < multiplier; j++) {
                 magnitude += freqByteData[offset + j];
+            }
             magnitude = magnitude / multiplier;
             var magnitude2 = freqByteData[i * multiplier];
-            analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
+            analyserContext.fillStyle = 'hsl( ' + Math.round((i * 360) / numBars) + ', 100%, 50%)';
             analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
         }
     }
 
-    rafID = window.requestAnimationFrame( updateAnalysers );
+    rafID = window.requestAnimationFrame(updateAnalysers);
 }
 
 function toggleMono() {
@@ -149,7 +153,7 @@ function toggleMono() {
         audioInput = realAudioInput;
     } else {
         realAudioInput.disconnect();
-        audioInput = convertToMono( realAudioInput );
+        audioInput = convertToMono(realAudioInput);
     }
 
     audioInput.connect(inputPoint);
@@ -167,40 +171,43 @@ function gotStream(stream) {
 
     analyserNode = audioContext.createAnalyser();
     analyserNode.fftSize = 2048;
-    inputPoint.connect( analyserNode );
+    inputPoint.connect(analyserNode);
 
-    audioRecorder = new Recorder( inputPoint );
+    audioRecorder = new Recorder(inputPoint);
 
     zeroGain = audioContext.createGain();
     zeroGain.gain.value = 0.0;
-    inputPoint.connect( zeroGain );
-    zeroGain.connect( audioContext.destination );
+    inputPoint.connect(zeroGain);
+    zeroGain.connect(audioContext.destination);
     updateAnalysers();
 }
 
 function initAudio() {
-    if (!navigator.getUserMedia)
+    if (!navigator.getUserMedia) {
         navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    if (!navigator.cancelAnimationFrame)
+    }
+    if (!navigator.cancelAnimationFrame) {
         navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-    if (!navigator.requestAnimationFrame)
+    }
+    if (!navigator.requestAnimationFrame) {
         navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+    }
 
     navigator.getUserMedia(
         {
-            "audio": {
-                "mandatory": {
-                    "googEchoCancellation": "false",
-                    "googAutoGainControl": "false",
-                    "googNoiseSuppression": "false",
-                    "googHighpassFilter": "false"
+            'audio': {
+                'mandatory': {
+                    'googEchoCancellation': 'false',
+                    'googAutoGainControl': 'false',
+                    'googNoiseSuppression': 'false',
+                    'googHighpassFilter': 'false'
                 },
-                "optional": []
+                'optional': []
             },
-        }, gotStream, function(e) {
+        }, gotStream, function (e) {
             alert('Error getting audio');
             console.log(e);
         });
 }
 
-window.addEventListener('load', initAudio );
+window.addEventListener('load', initAudio);
--- src/js/components/LoginComponent/recorder.js
+++ src/js/components/LoginComponent/recorder.js
@@ -1,95 +1,101 @@
 var myWorker = require('worker-loader!./recorderWorker.js');
 
-var Recorder = function(source, cfg){
-  var config = cfg || {};
-  var bufferLen = config.bufferLen || 4096;
-  this.context = source.context;
-  if(!this.context.createScriptProcessor){
-    this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
-  } else {
-    this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
-  }
-
-  var worker = new myWorker();
-  worker.postMessage({
-    command: 'init',
-    config: {
-      sampleRate: this.context.sampleRate
+var Recorder = function (source, cfg) {
+    var config = cfg || {};
+    var bufferLen = config.bufferLen || 4096;
+    this.context = source.context;
+    if (!this.context.createScriptProcessor) {
+        this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+    } else {
+        this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
     }
-  });
-  var recording = false,
-      currCallback;
 
-  this.node.onaudioprocess = function(e){
-    if (!recording) return;
+    var worker = new myWorker();
     worker.postMessage({
-      command: 'record',
-      buffer: [
-        e.inputBuffer.getChannelData(0),
-        e.inputBuffer.getChannelData(1)
-      ]
+        command: 'init',
+        config: {
+            sampleRate: this.context.sampleRate
+        }
     });
-  }
+    var recording = false,
+        currCallback;
 
-  this.configure = function(cfg){
-    for (var prop in cfg){
-      if (cfg.hasOwnProperty(prop)){
-        config[prop] = cfg[prop];
-      }
+    this.node.onaudioprocess = function (e) {
+        if (!recording) {
+            return;
+        }
+        worker.postMessage({
+            command: 'record',
+            buffer: [
+                e.inputBuffer.getChannelData(0),
+                e.inputBuffer.getChannelData(1)
+            ]
+        });
     }
-  }
 
-  this.record = function(){
-    recording = true;
-  }
+    this.configure = function (cfg) {
+        for (var prop in cfg) {
+            if (cfg.hasOwnProperty(prop)) {
+                config[prop] = cfg[prop];
+            }
+        }
+    };
 
-  this.stop = function(){
-    recording = false;
-  }
+    this.record = function () {
+        recording = true;
+    };
 
-  this.clear = function(){
-    worker.postMessage({ command: 'clear' });
-  }
+    this.stop = function () {
+        recording = false;
+    };
 
-  this.getBuffers = function(cb) {
-    currCallback = cb || config.callback;
-    worker.postMessage({ command: 'getBuffers' })
-  }
+    this.clear = function () {
+        worker.postMessage({command: 'clear'});
+    };
 
-  this.exportWAV = function(cb, type){
-    currCallback = cb || config.callback;
-    type = type || config.type || 'audio/wav';
-    if (!currCallback) throw new Error('Callback not set');
-    worker.postMessage({
-      command: 'exportWAV',
-      type: type
-    });
-  }
+    this.getBuffers = function (cb) {
+        currCallback = cb || config.callback;
+        worker.postMessage({command: 'getBuffers'})
+    };
 
-  this.exportMonoWAV = function(cb, type){
-    currCallback = cb || config.callback;
-    type = type || config.type || 'audio/wav';
-    if (!currCallback) throw new Error('Callback not set');
-    worker.postMessage({
-      command: 'exportMonoWAV',
-      type: type
-    });
-  }
+    this.exportWAV = function (cb, type) {
+        currCallback = cb || config.callback;
+        type = type || config.type || 'audio/wav';
+        if (!currCallback) {
+            throw new Error('Callback not set');
+        }
+        worker.postMessage({
+            command: 'exportWAV',
+            type: type
+        });
+    };
 
-  worker.onmessage = function(e){
-    var blob = e.data;
-    currCallback(blob);
-  }
+    this.exportMonoWAV = function (cb, type) {
+        currCallback = cb || config.callback;
+        type = type || config.type || 'audio/wav';
+        if (!currCallback) {
+            throw new Error('Callback not set');
+        }
+        worker.postMessage({
+            command: 'exportMonoWAV',
+            type: type
+        });
+    };
 
-  source.connect(this.node);
-  this.node.connect(this.context.destination);   // if the script node is not connected to an output the "onaudioprocess" event is not triggered in chrome.
+    worker.onmessage = function (e) {
+        var blob = e.data;
+        currCallback(blob);
+    };
+
+    source.connect(this.node);
+    this.node.connect(this.context.destination);   // if the script node is not connected to an output the 'onaudioprocess' event is not triggered in chrome.
 };
 
-Recorder.setupDownload = function(blob, filename){
-  var url = (window.URL || window.webkitURL).createObjectURL(blob);
-  var link = document.getElementById("save");
-  link.href = url;
-  link.download = filename || 'output.wav';
-}
+Recorder.setupDownload = function (blob, filename) {
+    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+    var link = document.getElementById('save');
+    link.href = url;
+    link.download = filename || 'output.wav';
+};
 
 module.exports = Recorder;
