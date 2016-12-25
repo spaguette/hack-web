45c5a6c272ef04da5aa0430183c885491534741d
ar-rapoport <arapoport@bitbucket.org>
2016-12-25 21:15:07 +0500

    поправлены bootstrap-файлы

--- script/bootstrap.cmd
+++ script/bootstrap.cmd
@@ -1,1 +1,1 @@
-echo 'Installing yarn globally' && npm i yarn@0.17.6 -q -g --no-progress --no-spin && echo 'Installing project npm dependencies' && npm run yarn
+echo 'Installing yarn globally' && npm i yarn@0.17.6 -q -g --no-progress --no-spin && echo 'Installing project npm dependencies' && yarn
